5614c49b713cfb1bd0e4748f475ba39c42e9b0a9
theghman <gerardomerida08@hotmail.com>
2018-03-14 15:05:13 -0700

    update

--- fileEncryptionLab/main.py
+++ fileEncryptionLab/main.py
@@ -1,13 +1,15 @@
-
-#my_private_key = rsa.generate_private_key(public_exponent = 65537, key_size=1024, backend=default_backend())
-#my_public_key = my_private_key.public_key()
-#fPrivKey = open("private.pem", "wb")
-#fPrivKey.write(my_private_key.exportKey('PEM'))
-#fPrivKey.close()
+#Various imports 
 import myEncryption
 import myDecryption
 import os
 import constants
+import RSAencrypt
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
+
+#Beginning of main
+#Need to separate this and make it prompt user 
 def main():
 	print ("###JPG file encrypter###")
 	message = raw_input("Please enter a message to encrypt. A random key will be generated for you.")
@@ -28,6 +30,24 @@ def main():
 			myDecryption.MyFileDecrypt(cipherText, iv, key, ext)
 		else:
 			print "File not found!"
-			
+	print ("Time to generate an RSA key:")
+	#Prompt user for path, then generate private key
+	RSA_key_path = raw_input("Please enter the path where you would like to save the key: ")
+	rsaPrivKey = rsa.generate_private_key(public_exponent=65537, key_size = 2048, backend = default_backend())
+	#Generate public key from privatekey
+	rsaPubKey = rsaPrivKey.public_key()
+	#Serialize keys for file
+	privPem = rsaPrivKey.private_bytes(encoding=serialization.Encoding.PEM, format = serialization.PrivateFormat.TraditionalOpenSSL, encryption_algorithm=serialization.NoEncryption())
+	pubPem = rsaPubKey.public_bytes(encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.SubjectPublicKeyInfo)
+	#Write keys to file
+	os.makedirs(RSA_key_path)
+	createFile = open(RSA_key_path+".pem", "wb")
+	createFile.write(privPem)
+	createFile.close()
+	createFile = open(RSA_key_path+".pub", "wb")
+	createFile.write(pubPem)
+	createFile.close()
+	RSACipher, cipher, IV, ext = RSAencrypt.myRSAEncrypt('gerardo.txt', RSA_key_path)
+#End of main()	
 if __name__=="__main__":
   main()

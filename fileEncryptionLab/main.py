0850feb7be812d4cc14492c637400cd4977e21c7
Gerardo <gerardo.merida@student.csulb.edu>
2018-03-15 18:04:01 +0000

    Fixed pathing errors and cleaned up comments

--- fileEncryptionLab/main.py
+++ fileEncryptionLab/main.py
@@ -8,17 +8,17 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.backends import default_backend
 
-#Beginning of main
-#Need to separate this and make it prompt user 
+#Beginning of main 
 def main():
 	#Prompt user until valid file is found or chooses to exit
 	encryptedFile = promptForFile()
 	if encryptedFile !="E":
 		cipherText, iv, key, ext = myEncryption.MyFileEncrypt(encryptedFile)
 		myDecryption.MyFileDecrypt(cipherText, iv, key, ext)
-	print ("Time to generate an RSA key:")
+	print ("RSA key generation")
+	print ("A public key and private key will be generated with file you provide.")
 	#Prompt user for path, then generate private key
-	RSA_key_path = input("Please enter the path where you would like to save the keys: ")
+	RSA_key_path = input("Please enter the path where you would like to save the keys and name: ")
 	rsaPrivKey = rsa.generate_private_key(public_exponent=65537, key_size = 2048, backend = default_backend())
 	#Generate public key from privatekey
 	rsaPubKey = rsaPrivKey.public_key()
@@ -27,8 +27,16 @@ def main():
 	pubPem = rsaPubKey.public_bytes(encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.SubjectPublicKeyInfo)
 	#Write keys to file
 	kPath, kName =os.path.split(RSA_key_path)
-	#os.makedirs(RSA_key_path)
-	os.makedirs(kPath)
+	print (kPath)
+	print (kName)
+	#Check if path is a directory, create it if it does not exist
+	if kPath != "":
+		os.makedirs(kPath)
+	#if there is no tail, add "default" to key path for key gen 
+	if kName == "":
+		print ("No name entered. default.pem, default.pub will be created in chosen directory")
+		RSA_key_path = RSA_key_path + "default"
+	#Write keys to files
 	createFile = open(RSA_key_path+".pem", "wb")
 	createFile.write(privPem)
 	createFile.close()

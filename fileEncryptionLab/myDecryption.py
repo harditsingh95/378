066fa5a51a427a47cf067531652244fca530d7fe
theghman <gerardomerida08@hotmail.com>
2018-04-03 15:32:46 -0700

    Update to add MAC
    Func calls remain same. Need to add error handling and test

--- fileEncryptionLab/myDecryption.py
+++ fileEncryptionLab/myDecryption.py
@@ -7,8 +7,12 @@ from cryptography.hazmat.primitives.ciphers import(Cipher, algorithms, modes)
 from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
-def MyDecrypt(cipherText,IV, key):
+def MyDecrypt(cipherText,IV, tag, key, hKey):
 	print ("Attempting to decrypt message...")
+	#Load hKey and verify it matches with the tag
+	hm = hmac.HMAC(kKey, hashes.SHA256(), backend=default_backend())
+	hm.update(cipherText)
+	hm.verify(tag)
 	#Generate plain text from given cipherText
 	plain = Cipher(algorithms.AES(key), modes.CBC(IV), backend=default_backend())
 	decryptor = plain.decryptor()
@@ -17,9 +21,9 @@ def MyDecrypt(cipherText,IV, key):
 	plainText = unpadder.update(plainText) + unpadder.finalize()
 	return plainText
 
-def MyFileDecrypt(cipher, iv, key, ext):
+def MyFileDecrypt(cipher, iv, tag, key, hKey, ext):
 	#Get plaintext from cipherText in MyDecrypt function
-	plainText = MyDecrypt(cipher, iv,key)
+	plainText = MyDecrypt(cipher, iv,tag, key ,hKey)
 	#Ask user for filename for new file
 	fileLoc = input("What would you like to save the file as? ")
 	newFile = fileLoc + ext
